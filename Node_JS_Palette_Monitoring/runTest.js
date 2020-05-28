2365ad302ec620df97a639bbe05094c609389dda
Vaibhav <vasinghm@in.ibm.com>
2020-05-28 16:34:13 +0530

    commit message

---
+++ Node_JS_Palette_Monitoring/runTest.js
@@ -0,0 +1,7 @@
+"use strict";
+// start script for starting jest tests
+const jest = require("jest");
+// tests only
+jest.run("--config jest.json");
+// tests + coverage
+// jest.run("--coverage --config jest.json");
